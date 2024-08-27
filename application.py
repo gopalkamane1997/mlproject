311e93d1159ad3d3aae4534ce4139ceb7332dd89
gopalkamane1997 <gopalkamane93@gmail.com>
2024-08-27 21:55:24 +0530

    pythonanywhere deploy

--- application.py
+++ application.py
@@ -34,6 +34,9 @@ def predict_datapoint():
         results = predict_pipeline.predict(pred_df)
         return render_template('home.html',results=results[0])
 
-if __name__ == "__main__":
-    app.run(host = '0.0.0.0')
+# if __name__ == "__main__":
+#     app.run(host = '0.0.0.0')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001, threaded=True)
         
